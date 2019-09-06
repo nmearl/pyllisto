fa60382abbfa17d233308c8db9f40337e48e378c
nmearl <contact@nicholasearl.me>
2019-09-06 17:26:13 -0400

    Prepare for voila refactor

--- js/scripts/copyfiles.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
-
-var fs = require('fs-extra');
-fs.copySync('static/', 'lib/', { filter: (src, dst) => /\.(css|less)$/ });
